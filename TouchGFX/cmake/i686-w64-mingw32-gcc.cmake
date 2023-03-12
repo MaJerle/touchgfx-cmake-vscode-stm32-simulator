f0a1225545a1b5a96523cd88e3bf328c01f989b8
Tilen Majerle <tilen.majerle@gmail.com>
2023-03-12 12:46:02 +0100

    Update template files

--- TouchGFX/cmake/i686-w64-mingw32-gcc.cmake
+++ TouchGFX/cmake/i686-w64-mingw32-gcc.cmake
@@ -1,7 +1,9 @@
-set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME               Windows)
 
 # Some default GCC settings
-set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_C_COMPILER                i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER              i686-w64-mingw32-g++)
+set(CMAKE_C_FLAGS                   "-Wall -Wextra -Wpedantic -Wno-unused-parameter")
+set(CMAKE_CXX_FLAGS                 "${CMAKE_C_FLAGS}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
